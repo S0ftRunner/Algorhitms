57750677667f1de4fc26c73ac666ec81ddd6d44e
S0ftRunner <benjamin.sokolov19@gmail.com>
2024-09-16 13:57:08 +0300

    add: new solution

--- codewars/deleteDigit.js
+++ codewars/deleteDigit.js
@@ -1,12 +1,19 @@
 /**
- * 
- * @param {number} n 
+ *
+ * @param {number} n
  */
 function deleteDigit(n) {
   const stringNumber = n.toString();
-  console.log(stringNumber.split(''));
+  if (stringNumber.length === 0) return n;
+  const numbersArray = [];
+  for (let i = 0; i < stringNumber.length; i++) {
+    numbersArray.push(
+      stringNumber.substring(0, i) +
+        stringNumber.substring(i + 1, stringNumber.length - 1)
+    );
+  }
+
+  console.log(numbersArray);
 }
 
-deleteDigit(1001);
-
-
+deleteDigit(1004);
