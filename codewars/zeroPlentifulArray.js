2d5d43508f6ba419ff4b617b0952a325cd3d9e13
S0ftRunner <benjamin.sokolov19@gmail.com>
2024-09-22 02:08:26 +0300

    add: solution

--- codewars/zeroPlentifulArray.js
+++ codewars/zeroPlentifulArray.js
@@ -4,17 +4,29 @@ function zeroPlentiful(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 0) {
       countZeros++;
-      if (arr.length - 1 === i && countZeros >= 4) ansArr.push(countZeros);
+      if (arr.length - 1 === i && countZeros >= 4 ) ansArr.push(countZeros);
+      if (arr.length - 1 === i && countZeros < 4) return 0;
     } else {
-      if (countZeros >= 4) ansArr.push(countZeros);
+      if (countZeros >= 4) {
+        ansArr.push(countZeros);
+      } else {
+        if (countZeros > 0) return 0;
+      }
       countZeros = 0;
     }
   }
 
-  for (let num of ansArr) {
-    console.log(num);
-  }
+
   return ansArr.length;
 }
 
-console.log(zeroPlentiful([0,2,0,0,0,0,3,4,5,0,0,0,0,0]));
+console.log(zeroPlentiful([0,0,0,0,1,0]));
+
+/**
+ * Лучшее решение:
+ *
+  function zeroPlentiful(arr){
+  var sequences = arr.map((x) => !x ? x : ',').join('').split(',').filter((str) => str);
+  return sequences.every((str) => str.length >= 4) ? sequences.length : 0;
+}
+ */
