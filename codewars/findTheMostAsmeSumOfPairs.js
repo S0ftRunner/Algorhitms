93e8a2451f100f31dcdb03204c67b411127a481e
Benjamin <benjamin.sokolov19@gmail.com>
2024-10-04 23:02:56 +0300

    add: solution

--- codewars/findTheMostAsmeSumOfPairs.js
+++ codewars/findTheMostAsmeSumOfPairs.js
@@ -18,11 +18,14 @@ function findPair(arr1, arr2) {
 
   // теперь ищем самый большой элемент в мапе:
 
+  console.log(mapArrays);
+
+
   let maxElement = mapArrays.get(arr1[0] + arr2[0]);
 
   let [finalElement, ...other] = mapArrays.keys();
   for (let [key, value] of mapArrays) {
-    if (maxElement < value) {
+    if (maxElement <= value) {
       maxElement = value;
       finalElement = key;
     }
@@ -35,7 +38,28 @@ function findPair(arr1, arr2) {
     }
   }
 
-  return finalResult.length ? finalResult : [];
+  return finalResult.length > 1 ? finalResult : [];
 }
 
-console.log(findPair([1, 2, 3, 4, 5], [0, 0, 0, 0, 0]));
+console.log(findPair([1,2,3,0,5,-2], [-1,2,-3,4,-5,6]));
+
+
+/**
+ * Мое решение работает для всех базовых тестов, но на динамических выдает ошибку
+ * Код, который прошел совершенно все тесты:
+  function findPair(arr1, arr2) {
+    var sum, pairs = {};
+    arr1.forEach(function(curr, i) {
+    sum = curr + arr2[i];
+      (pairs[sum] = pairs[sum] || []).push([curr, arr2[i]]);
+    });
+    var len, key, longest = [];
+    for (key in pairs) {
+      len = pairs[key].length; 
+      if (len > 1 && len >= longest.length) {
+        longest = pairs[key];
+      }
+    }
+  return longest;
+}
+ */
