480f3dc14ffac4856e73fe655992e5e3b34a31c3
S0ftRunner <benjamin.sokolov19@gmail.com>
2024-10-07 00:17:33 +0300

    add: rle solution

---
+++ codewars/objectDepth.js
@@ -0,0 +1,12 @@
+function depth(obj) {
+
+  if (Array.isArray(obj)) return 0;
+  let sum = 0;
+
+  const stack = Object.keys();
+  
+
+  
+  
+  return sum;
+}
--- codewars/rle.js
+++ codewars/rle.js
@@ -1,3 +1,25 @@
 var runLengthEncoding = function(str) {
-  
+  if (!str) return [];
+  let position = 0;
+  let symbol = str[0];
+  const resultArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== symbol) {
+      resultArr.push(package(symbol, i - position));
+      symbol = str[i];
+      position = i;
+    }
+  }
+
+  resultArr.push(package(symbol, str.length - position));
+
+  return resultArr;
 }
+
+
+function package(symbol, count) {
+  return [count, symbol];
+}
+
+console.log(runLengthEncoding("hello world!"));
