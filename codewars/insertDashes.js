ce81b9f4e721c14b1be3a442d9caa8b3b0469402
S0ftRunner <benjamin.sokolov19@gmail.com>
2025-01-15 09:55:01 +0300

    fix: доделать простую задачу

--- codewars/insertDashes.js
+++ codewars/insertDashes.js
@@ -1,4 +1,9 @@
 function insertDashII(num) {
+  const strNum = num.toString();
+
+  for (let i = 1, j = 0; i < strNum.length - 1, j < strNum.length; i++, j++) {
+    
+  }
 }
 
 console.log(insertDashII(454793));
