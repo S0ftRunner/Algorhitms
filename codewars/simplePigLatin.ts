0ade233ff2623d94d971cf9c03e45672fb07c9bc
Benjamin <benjamin.sokolov19@gmail.com>
2025-12-15 00:16:43 +0300

    add: few solutions

--- codewars/simplePigLatin.ts
+++ codewars/simplePigLatin.ts
@@ -1,9 +1,15 @@
 function simplePigLatin() {
-  const test = 'hello world';
+  const test = 'O emporatay oay oresmay !';
   // подается строка, где каждое слово должно трансформироваться по примеру
   // hello -> ellohay
   const strArray = test.split(' ');
-  const newArray = strArray.map(el => el.substring(1, el.length) + el.substring(0, 1) + 'ay');
+  const newArray = strArray.map(el =>{
+    if (/[a-z]/.test(el)) {
+       return el.substring(1, el.length) + el.substring(0, 1) + 'ay'
+    } else {
+      return el;
+    }
+  });
   console.log(newArray.join(' '));
   return newArray.join(' ');
 }
---
+++ leetcode/easy/longestCommonPrefix.ts
@@ -0,0 +1,25 @@
+/**
+ * функция принимает массив строк, должна вернуть наибольшую общую подстроку, иначе вернуть пустую строку
+ *
+ */
+function longestCommonPrefix(strs: string[]): string {
+  if (!strs.length) {
+    return "";
+  }
+
+  strs.sort();
+  const first = strs[0],
+    last = strs[strs.length - 1];
+
+  let result = "";
+
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] === last[i]) {
+      result += first[i];
+    } else {
+      break;
+    }
+  }
+
+  return result;
+}
