eb109777d290d641a470e0a9d74e835bb458744b
Benjamin <benjamin.sokolov19@gmail.com>
2025-12-24 10:57:16 +0300

    add: solution

---
+++ leetcode/easy/validNonAlpanumericPalindrome.ts
@@ -0,0 +1,6 @@
+function isPalindrome(s: string): boolean {
+   const newStr = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return newStr === newStr.split('').reverse().join('');
+}
+
+console.log(isPalindrome('0P'));
